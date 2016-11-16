3110687d1df8cb377c2f9c7fd5832fb6e3db5b35
Nikolaj Čolić <nikolaj.colic@gmail.com>
2016-11-16 19:25:48 +0100

    Dodal check_email.php
    Dodal sem check_email.php datoteko v kateri sem napisal skripto, ki
    preveri če email obstaja v bazi in vrne string. Če mail obstaja vrne
    mail, drugače pa vrne string "Neobstaja". Ta php skripta je potrebna pri
    registraciji.

---
+++ check_email.php
@@ -0,0 +1,18 @@
+<?php
+
+// Send variables for the MySQL database class.
+$link = mysql_connect('localhost', 'sola_voznje', 'sola_voznje') or die('Could not connect: ' . mysql_error());
+mysql_select_db('sola_voznje') or die('Could not select database');
+
+$email = mysql_real_escape_string($_GET['email'], $link);
+$query = "SELECT * FROM `uporabnik` WHERE email='$email'";
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+$num_results = mysql_num_rows($result);
+if ( $num_results== 1) {
+    $row = mysql_fetch_array($result);
+    echo $row['email'] ."\n";
+}else{
+    echo 'Neobstaja!';
+}
+?>
