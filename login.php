e65dd4cebcdd3be559c34804a3da0a4670dfac6e
Nikolaj Čolić <nikolaj.colic@gmail.com>
2016-11-18 21:52:57 +0100

    Login in registration update
    V registration.php sem dodal, da ko se uporabnik vnaša v bazo se njegovo
    geslo zakodira z sha1 metodo. Enako sem naredil pri prijavi, ko iščemo
    če obstaja uporabnik, moramo prvo geslo zakodirati z sha1, da lahko
    primerjamo z rezultati v bazi.

--- login.php
+++ login.php
@@ -6,6 +6,7 @@ mysql_select_db('sola_voznje') or die('Could not select database');
 
 $email = mysql_real_escape_string($_GET['email'], $link);
 $geslo = mysql_real_escape_string($_GET['geslo'], $link);
+$geslo = sha1($geslo);
 $query = "SELECT * FROM `uporabnik` WHERE email='$email' AND geslo='$geslo'";
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
